5e955dee768c84d9e0e53b566b59540c2bf06b4f
Ryan Joseph <ryan@electricsheep.co>
2024-02-11 21:35:38 -0800

    .

--- theburgbot/httpapi.py
+++ theburgbot/httpapi.py
@@ -1,13 +1,48 @@
 import logging
 import threading
 
+import aiosqlite
 import chevron
 from aiohttp import web
 
 from theburgbot import constants
+from theburgbot.common import dprint as print
 from theburgbot.db import TheBurgBotDB, audit_log_start_end_async
+from twentywordmtg.populate import mtgjson_sqlite_path
 
 LOGGER = logging.getLogger("discord")
+
+
+async def twentywordmagic_cards(req: web.Request):
+    print(req.match_info["card_name"])
+    db_path = await mtgjson_sqlite_path()
+    print(db_path)
+    async with aiosqlite.connect(db_path) as db:
+        db.row_factory = aiosqlite.Row
+        records = await db.execute_fetchall(
+            "select * from cards left join twentyword_cards as tw "
+            + "where cards.uuid = tw.card_uuid and cards.name like ?",
+            (req.match_info["card_name"],),
+        )
+        await db.commit()
+        print(records[0])
+        print(records[0].keys())
+        print(records[0]["text"])
+
+        html_out = ""
+        frag_str = None
+        with open("templates/twmtg_card_frag.html", "r") as frag:
+            frag_str = frag.read()
+        for row in records:
+            row_copy = {**row}
+            row_copy["text"] = row_copy["text"].replace("\\n", "<br/>")
+            if row_copy["legal"]:
+                row_copy["TMPL_legal"] = [True]
+            print(row_copy)
+            html_out += chevron.render(frag_str, row_copy)
+
+        print(html_out)
+        return web.Response(text=html_out, content_type="text/html")
 
 
 class TheBurgBotHTTP:
@@ -35,8 +70,10 @@ class TheBurgBotHTTP:
                     f"/{constants.USER_STATIC_HTTP_PATH}/" "{doc_id}",
                     self.get_static_route_handler,
                 ),
+                web.get("/twentywordmagic/card/{card_name}", twentywordmagic_cards),
             ]
         )
+        print(self.app)
         self.thread = threading.Thread(target=self.thread_runner, daemon=True)
         self.thread.start()
 
